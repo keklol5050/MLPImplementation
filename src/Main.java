fb301bd9c343fe7f438c1a81c410abab990279a6
keklol5050 <kek.lol5050@gmail.com>
2024-04-22 11:59:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.awt.GraphicsEnvironment;
+public class Main{
+    public static void main (String [] args) throws IOException{
+        Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
+        }else{
+            Solution.main(new String[0]);
+            System.out.println("Type 'exit' to close the console");
+        }
+    }
+}
